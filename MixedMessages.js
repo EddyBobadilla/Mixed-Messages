7b30a1769a3c1acf61742ca77111b8e4aa37bc5a
Eddy Bobadilla <eddybobadilla91@gmail.com>
2021-11-02 15:43:00 -0700

    Created Astrological Sign Array

---
+++ MixedMessages.js
@@ -0,0 +1,4 @@
+//User input return random daily horoscope
+const 
+
+const astroSign = ['Aries', 'Taurus', 'Gemini', 'Cancer', 'Leo', 'Virgo', 'Libra', 'Scorpio', 'Sagittarius', 'Capicorn', 'Aquarius', 'Pisces']
